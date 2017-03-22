e2c942c9627ceb6a7c95e7172a95fa65b37bbfb2
since4 <rol123ben@outlook.de>
2017-03-23 00:18:09 +0100

    ng g c Demo-Temp; temporary folder for tested code to use somewhere else. Tables, Tooltips, DragDrop etc.

---
+++ src/app/demo-temp/demo-temp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-temp',
+  templateUrl: 'demo-temp.component.html',
+  styleUrls: ['demo-temp.component.css']
+})
+export class DemoTempComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
