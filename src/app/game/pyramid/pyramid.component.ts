05f73b7bd010eca415541a68388df36ff5b325a0
snoweZz <vincent@rueegge.ch>
2017-03-27 17:00:59 +0200

    Icon hover fixed

--- src/app/game/pyramid/pyramid.component.ts
+++ src/app/game/pyramid/pyramid.component.ts
@@ -10,6 +10,20 @@ export class PyramidComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //styles in styles.css at the end under popover, to style:
+    //.popover .popover-title .popover-content .arrow
+
+    //hover from top doesn't work properly
+    (<any>$('#pyramid')).popover({
+      placement: 'top',
+      trigger : 'hover',
+      toggle: 'popover',
+      title: '',
+      show: 5500,
+      hide: 5500,
+      html: true,
+      content : '<img height="50%" width="100%" src="../../../../../assets/images/cards/pyramiddec.png"/>'
+    });
   }
 
 }
