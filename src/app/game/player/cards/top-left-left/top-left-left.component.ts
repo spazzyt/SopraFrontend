b1b65ae23d7001c9d3836d83d274d8b7a87df1eb
davwys <davidwyss@dhcp-wlan-uzh-10-12-139-012.uzh.ch>
2017-03-27 14:49:17 +0200

    added first card tooltip

--- src/app/game/player/cards/top-left-left/top-left-left.component.ts
+++ src/app/game/player/cards/top-left-left/top-left-left.component.ts
@@ -10,6 +10,33 @@ export class TopLeftLeftComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    //styles in styles.css at the end under popover, to style:
+    //.popover .popover-title .popover-content .arrow
+
+
+    (<any>$('#test')).popover({
+      placement: 'top',
+      trigger : 'hover',
+      toggle: 'popover',
+      title: 'Title Whatever you Need',
+      show: 5500,
+      hide: 5500,
+      html: true,
+      content : '<div><b>Very</b> <span style="color: #C21F">Dynamic</span> ' +
+      '<span style="color: #00b3ee">Reusable</span>' +
+      '<b><i><span style="color: #ffc520">Tooltip With</span></i></b> ' +
+      '<small>Html support</small>.' +
+      '<img src="../../../../../assets/images/cards/pyramiddec.png"/></div>'
+    });
+
+
+
+    (<any>$('#test')).css('color','#ffc520');
+
+
+
+
   }
 
 }
