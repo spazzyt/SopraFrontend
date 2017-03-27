dab49d4fcd8e9c74f6e18ed0d468038d537d55b4
snoweZz <vincent@rueegge.ch>
2017-03-27 17:00:59 +0200

    Icon hover fixed

--- src/app/game/player/cards/top-middle-right/top-middle-right.component.ts
+++ src/app/game/player/cards/top-middle-right/top-middle-right.component.ts
@@ -10,6 +10,26 @@ export class TopMiddleRightComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //styles in styles.css at the end under popover, to style:
+    //.popover .popover-title .popover-content .arrow
+
+    //hover from top doesn't work properly
+    (<any>$('#tmr')).popover({
+      placement: 'top',
+      trigger : 'hover',
+      toggle: 'popover',
+      title: '',
+      show: 5500,
+      hide: 5500,
+      html: true,
+      content : '<img height="50%" width="100%" src="../../../../../assets/images/cards/burialdec.png"/>'
+    });
+
+
+
+    //(<any>$('#card')).css('color','#ffc520'); change the color of a link
+
+
   }
 
 }
