bdb92d2f2db97ed2c41b29f5ae3ef9934b2b6233
since4 <rol123ben@outlook.de>
2017-03-29 06:17:47 +0200

    dragula installed: dragula-demo added.

--- src/app/shared/models/ship.ts
+++ src/app/shared/models/ship.ts
@@ -1,9 +1,27 @@
 import {ShipSlot} from "./ship-slot";
-export class Ship {
-  slots:ShipSlot[] = [];
-  imageUrl:string;
 
-  constructor(public size:number, public color?:string){
-    this.imageUrl = "../../../assets/images/ship_"+size+".png"
+export class Ship {
+
+  //unique identifier
+  public id:number;
+
+  //number of slots
+  public size:number;
+
+  //minimum number of stones needed to sail
+  public minStones:number;
+
+  //array of slot objects
+  public slots:ShipSlot[] = [];
+
+  //ship image
+  public imageUrl:string;
+
+  constructor(id_:number, size_:number, minStones_?:number){
+    //initialize attributes
+    this.id=id_;
+    this.size=size_;
+    this.minStones=minStones_;
+    this.imageUrl = "../../../assets/images/ship_" + size_ + ".png"
   }
 }
