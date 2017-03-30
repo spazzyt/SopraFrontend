bf0e21e543d7acae6903da4310ac9a949d055f35
since4 <rol123ben@outlook.de>
2017-03-30 18:34:31 +0200

    dragula demo: when dragged no further dragging allowed works, drag stones from supply sled to ship slots works, copy stones from supply sled works

--- src/app/demo-temp/dragula-demo/dragula-demo.component.ts
+++ src/app/demo-temp/dragula-demo/dragula-demo.component.ts
@@ -62,9 +62,7 @@ export class DragulaDemoComponent implements OnInit {
 
 
     //ex.10 (dragula does not work, only ng2-dragula)
-    //dragula([document.querySelector('left'), document.querySelector('right')]);
-    //dragula([document.querySelector('#left'), document.querySelector('#right')]);
-    //dragula([document.querySelector('.left'), document.querySelector('.right')]);
+
     dragulaService.setOptions('harbours_bag', {
       accepts: function (el, target, source, sibling) {
         console.log("10.1.1 ", `el: ${el}`); /*moved element*/
@@ -79,24 +77,173 @@ export class DragulaDemoComponent implements OnInit {
         console.log("10.3.2 ", `target.id: ${target.id}`); /*its id */
         console.log("10.3.3 ", `target.innerHTML: ${target.innerHTML}`); /*its innerHTML*/
 
-        console.log("10.3.4 ", `target.parentElement: ${target.parentElement}`); /**/
-        console.log("10.3.5 ", `target.parentElement.id: ${target.parentElement.id}`); /**/
+        console.log("10.4.1 ", `target.parentElement: ${target.parentElement}`); /**/
+        console.log("10.4.2 ", `target.parentElement.id: ${target.parentElement.id}`); /**/
 
         let isEmpty = target.innerHTML === "";
         let isArrivingHarbour = target.parentElement.id === "arriving_harbours";
-        console.log("10.3.6 ", "isEmpty: " ,isEmpty); /**/
-        console.log("10.3.7 ", "isArrivingHarbour: " ,isArrivingHarbour); /**/
+        console.log("10.5.1 ", "isEmpty: " ,isEmpty); /**/
+        console.log("10.5.2 ", "isArrivingHarbour: " ,isArrivingHarbour); /**/
+
         if(isEmpty && isArrivingHarbour) {
-          console.log("10.3.8 ", "=====TRUE====="); /**/
+          console.log("10.6.1 ", "====HARBOUR=TRUE====="); /**/
           return true;
         }
         else{
-          console.log("10.3.9 ", "=====FALSE====="); /**/
+          console.log("10.6.2 ", "====HARBOUR=FALSE====="); /**/
           return false;
         }
         //return (target.id != "arriving_harbour_2"); /*disallows drop into arriving_harbour_2 */
       },
-      //copy: true /*copies el to target and leaves it in source*/
+
+      moves: function (el, source, handle, sibling) {
+        return true; /* true: elements are always draggable by default */
+        //return el.tagName.toLowerCase() === 'mvp-navigation-item'; /*only move favorite items, not the icon element */
+      },
+
+      isContainer: function (el) {
+        return false;  /* only elements in drake.containers will be taken into account */
+      },
+
+      invalid: function (el, handle) {
+
+        let isArrivingHarbour = el.parentElement.parentElement.id === "arriving_harbours";
+        let isInvalid = isArrivingHarbour;
+
+        if(isInvalid) {
+          console.log("10.10.1 ", "-----TRUE-----"); /**/
+          return true;
+        }
+        else{
+          console.log("10.10.2 ", "-----FALSE-----"); /**/
+          return false; /* false: don't prevent any drags from initiating by default */
+        }
+
+
+      },
+
+      direction: 'vertical',  /*X (horizontal) or Y axis (vertical) is considered when determining where an element would be dropped*/
+
+      //copy: false, /*false: elements are moved by default, not copied. true: copies el to target and leaves it in source*/
+      copy:function (el, source) {
+
+        let isCopyClass = el.classList.contains('you-may-copy-us'); /*the css class name*/
+        let isCopyId = el.id === '#you-may-copy-me'; /*the css id; but use a className for this*/
+        console.log("10.15.1 ", `el.id: ${el.id}`); /*moved element*/
+        console.log("10.15.2 ", `el.className: ${el.className}`); /* */
+        console.log("10.15.1 ", `el.isCopyClass: ${isCopyClass}`); /* */
+        console.log("10.15.2 ", `el.isCopyId: ${isCopyId}`); /* */
+
+        if(isCopyClass || isCopyId) {
+          console.log("10.16.1 ", "-----TRUE-----"); /**/
+          return true;
+        }
+        else{
+          console.log("10.16.2 ", "-----FALSE-----"); /**/
+          return false;
+        }
+
+      },
+
+      revertOnSpill: false, /* spilling will put the element back where it was dragged from, if this is true*/
+
+      removeOnSpill: false, /* spilling will `.remove` the element, if this is true */
+
+    });
+
+
+    //ex.11
+    dragulaService.setOptions('stone_slots_bag', {
+      accepts: function (el, target, source, sibling) {
+        console.log("11.1.1 ", `el: ${el}`); /*moved element*/
+        console.log("11.1.2 ", `el.id: ${el.id}`); /*its id*/
+
+        console.log("11.2.1 ", `source: ${source}`); /*source element */
+        console.log("11.2.2 ", `source.id: ${source.id}`); /*its id*/
+
+        console.log("11.2.1 ", `sibling: ${sibling}`); /**/
+
+        console.log("11.3.1 ", `target: ${target}`); /*target element*/
+        console.log("11.3.2 ", `target.id: ${target.id}`); /*its id */
+        console.log("11.3.3 ", `target.innerHTML: ${target.innerHTML}`); /*its innerHTML*/
+
+        console.log("11.4.1 ", `target.parentElement: ${target.parentElement}`); /**/
+        console.log("11.4.2 ", `target.parentElement.id: ${target.parentElement.id}`); /**/
+
+        let isEmpty = target.innerHTML === "";
+        let isStoneSlot_1 = target.parentElement.id === "ship_1_slots";
+        let isStoneSlot_2 = target.parentElement.id === "ship_2_slots";
+        let isStoneSlot_3 = target.parentElement.id === "ship_3_slots";
+        let isStoneSlot_4 = target.parentElement.id === "ship_4_slots";
+        console.log("11.5.1 ", "isEmpty: " ,isEmpty); /**/
+        console.log("11.5.2 ", "isStoneSlot_1: " ,isStoneSlot_1); /**/
+        console.log("11.5.3 ", "isStoneSlot_2: " ,isStoneSlot_2); /**/
+        console.log("11.5.4 ", "isStoneSlot_3: " ,isStoneSlot_3); /**/
+        console.log("11.5.5 ", "isStoneSlot_4: " ,isStoneSlot_4); /**/
+
+        if(isEmpty && (isStoneSlot_1 || isStoneSlot_2 || isStoneSlot_3 || isStoneSlot_4 )) {
+          console.log("11.6.1 ", "====SLOT=TRUE====="); /**/
+          return true;
+        }
+        else{
+          console.log("11.6.2 ", "====SLOT=FALSE====="); /**/
+          return false;
+        }
+        //return (target.id != "arriving_harbour_2"); /*disallows drop into arriving_harbour_2 */
+      },
+
+      moves: function (el, source, handle, sibling) {
+        return true; /* true: elements are always draggable by default */
+        //return el.tagName.toLowerCase() === 'mvp-navigation-item'; /*only move favorite items, not the icon element */
+      },
+
+      isContainer: function (el) {
+        return false;  /* only elements in drake.containers will be taken into account */
+      },
+
+      invalid: function (el, handle) {
+
+        let isStoneSlot = el.parentElement.parentElement.classList.contains('ship_slots');
+        let isInvalid = isStoneSlot;
+
+        if(isInvalid) {
+          console.log("10.10.1 ", "-----TRUE-----"); /**/
+          return true;
+        }
+        else{
+          console.log("10.10.2 ", "-----FALSE-----"); /**/
+          return false; /* false: don't prevent any drags from initiating by default */
+        }
+
+      },
+
+      direction: 'vertical',  /*X (horizontal) or Y axis (vertical) is considered when determining where an element would be dropped*/
+
+      //copy: true, /*false: elements are moved by default, not copied. true: copies el to target and leaves it in source*/
+      copy:function (el, source) {
+
+        let isCopyClass = el.classList.contains('you-may-copy-us'); /*the css class name*/
+        let isCopyId = el.id === '#you-may-copy-me'; /*the css id; but use a className for this*/
+        console.log("11.15.1 ", `el.id: ${el.id}`); /*moved element*/
+        console.log("11.15.2 ", `el.className: ${el.className}`); /* */
+        console.log("11.15.1 ", `el.isCopyClass: ${isCopyClass}`); /* */
+        console.log("11.15.2 ", `el.isCopyId: ${isCopyId}`); /* */
+
+        if(isCopyClass || isCopyId) {
+          console.log("11.16.1 ", "----COPY-TRUE-----"); /**/
+          return true;
+        }
+        else{
+          console.log("11.16.2 ", "----COPY-FALSE-----"); /**/
+          return false;
+        }
+
+      },
+
+      revertOnSpill: false, /* spilling will put the element back where it was dragged from, if this is true*/
+
+      removeOnSpill: false, /* spilling will `.remove` the element, if this is true */
+
     });
 
 
