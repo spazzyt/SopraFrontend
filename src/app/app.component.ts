72c917414d0fa5f42c14170519d75ee31663ecf8
since4 <rol123ben@outlook.de>
2017-04-02 16:36:02 +0200

    local storage (workaround1: used to reload lobby, prevent infinit reloading)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {LobbyComponent} from "./lobby/lobby.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sopra FS17';
+
+  //===========================================
+  // Enable Communication with Child Components
+  //===========================================
+
+  // Enable communication with MarketComponent
+  @ViewChild(LobbyComponent) lobbyComponent:LobbyComponent;
+
+  //================
+  // ngAfterViewInit
+  //================
+
+  ngAfterViewInit() {
+
+  }
+
 }
