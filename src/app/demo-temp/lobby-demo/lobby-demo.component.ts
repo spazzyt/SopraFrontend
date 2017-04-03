b8c2d044a7fe29f4de7d07f0967f9a49c50fb20e
since4 <rol123ben@outlook.de>
2017-04-03 13:24:59 +0200

    model user vs lobby-demo component: inconsistencies fixed.

--- src/app/demo-temp/lobby-demo/lobby-demo.component.ts
+++ src/app/demo-temp/lobby-demo/lobby-demo.component.ts
@@ -7,12 +7,12 @@ import {Game} from "../../shared/models/game";
 
 //Dummy data USERS, GAMES, MYSELF
 const USERS: User[] = [
-  {"id":1,"username":"sgfsdg","token":"20532346-150d-460f-829c-041f6fe1da2a","status":"OFFLINE","games":[],"moves":[]},
-  {"id":2,"username":"rtwet","token":"7fb9330f-9ebe-4d6d-b182-679c875b43aa","status":"OFFLINE","games":[],"moves":[]},
-  {"id":3,"username":"dfjoie","token":"704b639d-3d9e-4e61-a780-f7ee5f898e55","status":"OFFLINE","games":[],"moves":[]},
-  {"id":4,"username":"Roland123","token":"f49e8030-0ef8-45a7-9c42-80eee85ddfd1","status":"OFFLINE","games":[],"moves":[]},
-  {"id":5,"username":"hsdosdf","token":"aa8ef17c-7392-4b5c-bf91-c3132e588eaf","status":"OFFLINE","games":[],"moves":[]},
-  {"id":6,"username":"Zulsdao","token":"b3a4af45-5578-4d70-8df8-26d4f40031da","status":"OFFLINE","games":[],"moves":[]}
+  {"oldGames":null,"decisions":null,"id":1,"username":"sgfsdg","token":"20532346-150d-460f-829c-041f6fe1da2a","status":"OFFLINE","games":[],"moves":[]},
+  {"oldGames":null,"decisions":null,"id":2,"username":"rtwet","token":"7fb9330f-9ebe-4d6d-b182-679c875b43aa","status":"OFFLINE","games":[],"moves":[]},
+  {"oldGames":null,"decisions":null,"id":3,"username":"dfjoie","token":"704b639d-3d9e-4e61-a780-f7ee5f898e55","status":"OFFLINE","games":[],"moves":[]},
+  {"oldGames":null,"decisions":null,"id":4,"username":"Roland123","token":"f49e8030-0ef8-45a7-9c42-80eee85ddfd1","status":"OFFLINE","games":[],"moves":[]},
+  {"oldGames":null,"decisions":null,"id":5,"username":"hsdosdf","token":"aa8ef17c-7392-4b5c-bf91-c3132e588eaf","status":"OFFLINE","games":[],"moves":[]},
+  {"oldGames":null,"decisions":null,"id":6,"username":"Zulsdao","token":"b3a4af45-5578-4d70-8df8-26d4f40031da","status":"OFFLINE","games":[],"moves":[]}
   ];
 
 const GAMES: Game[] = [{"id":1,"name":"sgfsdg","owner":"sgfsdg","status":null,"currentPlayer":null,"numPlayers":4,"moves":[],"players":[]},
