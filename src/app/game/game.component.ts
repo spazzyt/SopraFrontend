c90da07ce0b3966fc3065b2f7b9abb8f1842af77
since4 <rol123ben@outlook.de>
2017-04-04 06:02:54 +0200

    change dragula

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -310,31 +310,35 @@ export class GameComponent  implements OnInit {
     this.dragulaService.setOptions('harbours_bag', {
       accepts: function (el, target, source, sibling) {
 
-        let isEmpty = target.innerHTML === "";
-        let isArrivingHarbour = target.parentElement.id === "arriving_harbours";
+        if (target){
 
-        /*Debugging
-         console.log("10.1.1 ", `el: ${el}`); //moved element
-         console.log("10.1.2 ", `el.id: ${el.id}`); //its id
-         console.log("10.2.1 ", `source: ${source}`); //source element
-         console.log("10.2.2 ", `source.id: ${source.id}`); //its id
-         console.log("10.2.1 ", `sibling: ${sibling}`);
-         console.log("10.3.1 ", `target: ${target}`); //target element
-         console.log("10.3.2 ", `target.id: ${target.id}`); //its id
-         console.log("10.3.3 ", `target.innerHTML: ${target.innerHTML}`); //its innerHTML
-         console.log("10.4.1 ", `target.parentElement: ${target.parentElement}`);
-         console.log("10.4.2 ", `target.parentElement.id: ${target.parentElement.id}`);
-         console.log("10.5.1 ", "isEmpty: " ,isEmpty);
-         console.log("10.5.2 ", "isArrivingHarbour: " ,isArrivingHarbour);
-         */
+          let isEmpty = target.innerHTML === "";
+          let isArrivingHarbour = target.parentElement.id === "arriving_harbours";
 
-        if (isEmpty && isArrivingHarbour) {
-          console.log("10.6.1 ", "====HARBOUR=TRUE=====");
-          return true;
-        }
-        else {
-          console.log("10.6.2 ", "====HARBOUR=FALSE=====");
-          return false;
+
+          /*Debugging
+           console.log("10.1.1 ", `el: ${el}`); //moved element
+           console.log("10.1.2 ", `el.id: ${el.id}`); //its id
+           console.log("10.2.1 ", `source: ${source}`); //source element
+           console.log("10.2.2 ", `source.id: ${source.id}`); //its id
+           console.log("10.2.1 ", `sibling: ${sibling}`);
+           console.log("10.3.1 ", `target: ${target}`); //target element
+           console.log("10.3.2 ", `target.id: ${target.id}`); //its id
+           console.log("10.3.3 ", `target.innerHTML: ${target.innerHTML}`); //its innerHTML
+           console.log("10.4.1 ", `target.parentElement: ${target.parentElement}`);
+           console.log("10.4.2 ", `target.parentElement.id: ${target.parentElement.id}`);
+           console.log("10.5.1 ", "isEmpty: " ,isEmpty);
+           console.log("10.5.2 ", "isArrivingHarbour: " ,isArrivingHarbour);
+           */
+
+          if (isEmpty && isArrivingHarbour) {
+            console.log("10.6.1 ", "====HARBOUR=TRUE=====");
+            return true;
+          }
+          else {
+            console.log("10.6.2 ", "====HARBOUR=FALSE=====");
+            return false;
+          }
         }
       },
 
@@ -348,18 +352,20 @@ export class GameComponent  implements OnInit {
 
       invalid: function (el, handle) {
 
-        let isArrivingHarbour = el.parentElement.parentElement.id === "arriving_harbours";
-        let isInvalid = isArrivingHarbour;
+        if (el) {
 
-        if (isInvalid) {
-          console.log("10.10.1 ", "-----TRUE-----");
-          return true;
-        }
-        else {
-          console.log("10.10.2 ", "-----FALSE-----");
-          return false; //false: don't prevent any drags from initiating by default
-        }
+          let isArrivingHarbour = el.parentElement.parentElement.id === "arriving_harbours";
+          let isInvalid = isArrivingHarbour;
 
+          if (isInvalid) {
+            console.log("10.10.1 ", "-----TRUE-----");
+            return true;
+          }
+          else {
+            console.log("10.10.2 ", "-----FALSE-----");
+            return false; //false: don't prevent any drags from initiating by default
+          }
+        }
 
       },
 
@@ -367,23 +373,26 @@ export class GameComponent  implements OnInit {
 
       copy: function (el, source) {
 
-        let isCopyClass = el.classList.contains('you-may-copy-us'); //the css class name
-        let isCopyId = el.id === '#you-may-copy-me'; //the css id; but use a className for this
+        if (el) {
 
-        /*Debugging
-         console.log("10.15.1 ", `el.id: ${el.id}`); //moved element
-         console.log("10.15.2 ", `el.className: ${el.className}`);
-         console.log("10.15.1 ", `el.isCopyClass: ${isCopyClass}`);
-         console.log("10.15.2 ", `el.isCopyId: ${isCopyId}`);
-         */
+          let isCopyClass = el.classList.contains('you-may-copy-us'); //the css class name
+          let isCopyId = el.id === '#you-may-copy-me'; //the css id; but use a className for this
 
-        if (isCopyClass || isCopyId) {
-          console.log("10.16.1 ", "-----TRUE-----");
-          return true;
-        }
-        else {
-          console.log("10.16.2 ", "-----FALSE-----");
-          return false;
+          /*Debugging
+           console.log("10.15.1 ", `el.id: ${el.id}`); //moved element
+           console.log("10.15.2 ", `el.className: ${el.className}`);
+           console.log("10.15.1 ", `el.isCopyClass: ${isCopyClass}`);
+           console.log("10.15.2 ", `el.isCopyId: ${isCopyId}`);
+           */
+
+          if (isCopyClass || isCopyId) {
+            console.log("10.16.1 ", "-----TRUE-----");
+            return true;
+          }
+          else {
+            console.log("10.16.2 ", "-----FALSE-----");
+            return false;
+          }
         }
 
       },
@@ -400,39 +409,41 @@ export class GameComponent  implements OnInit {
 
       accepts: function (el, target, source, sibling) {
 
-        let isEmpty = target.innerHTML === "";
-        let isStoneSlot_1 = target.parentElement.id === "ship_1_slots";
-        let isStoneSlot_2 = target.parentElement.id === "ship_2_slots";
-        let isStoneSlot_3 = target.parentElement.id === "ship_3_slots";
-        let isStoneSlot_4 = target.parentElement.id === "ship_4_slots";
+        if (target) {
 
-        /*Debugging:
-         console.log("11.1.1 ", `el: ${el}`); //moved element
-         console.log("11.1.2 ", `el.id: ${el.id}`); //its id
-         console.log("11.2.1 ", `source: ${source}`); //source element
-         console.log("11.2.2 ", `source.id: ${source.id}`); //its id
-         console.log("11.2.1 ", `sibling: ${sibling}`);
-         console.log("11.3.1 ", `target: ${target}`); //target element
-         console.log("11.3.2 ", `target.id: ${target.id}`); //its id
-         console.log("11.3.3 ", `target.innerHTML: ${target.innerHTML}`); //its innerHTML
-         console.log("11.4.1 ", `target.parentElement: ${target.parentElement}`);
-         console.log("11.4.2 ", `target.parentElement.id: ${target.parentElement.id}`);
-         console.log("11.5.1 ", "isEmpty: " ,isEmpty);
-         console.log("11.5.2 ", "isStoneSlot_1: " ,isStoneSlot_1);
-         console.log("11.5.3 ", "isStoneSlot_2: " ,isStoneSlot_2);
-         console.log("11.5.4 ", "isStoneSlot_3: " ,isStoneSlot_3);
-         console.log("11.5.5 ", "isStoneSlot_4: " ,isStoneSlot_4);
-         */
+          let isEmpty = target.innerHTML === "";
+          let isStoneSlot_1 = target.parentElement.id === "ship_1_slots";
+          let isStoneSlot_2 = target.parentElement.id === "ship_2_slots";
+          let isStoneSlot_3 = target.parentElement.id === "ship_3_slots";
+          let isStoneSlot_4 = target.parentElement.id === "ship_4_slots";
 
-        if (isEmpty && (isStoneSlot_1 || isStoneSlot_2 || isStoneSlot_3 || isStoneSlot_4 )) {
-          console.log("11.6.1 ", "====SLOT=TRUE=====");
-          return true;
+          /*Debugging:
+           console.log("11.1.1 ", `el: ${el}`); //moved element
+           console.log("11.1.2 ", `el.id: ${el.id}`); //its id
+           console.log("11.2.1 ", `source: ${source}`); //source element
+           console.log("11.2.2 ", `source.id: ${source.id}`); //its id
+           console.log("11.2.1 ", `sibling: ${sibling}`);
+           console.log("11.3.1 ", `target: ${target}`); //target element
+           console.log("11.3.2 ", `target.id: ${target.id}`); //its id
+           console.log("11.3.3 ", `target.innerHTML: ${target.innerHTML}`); //its innerHTML
+           console.log("11.4.1 ", `target.parentElement: ${target.parentElement}`);
+           console.log("11.4.2 ", `target.parentElement.id: ${target.parentElement.id}`);
+           console.log("11.5.1 ", "isEmpty: " ,isEmpty);
+           console.log("11.5.2 ", "isStoneSlot_1: " ,isStoneSlot_1);
+           console.log("11.5.3 ", "isStoneSlot_2: " ,isStoneSlot_2);
+           console.log("11.5.4 ", "isStoneSlot_3: " ,isStoneSlot_3);
+           console.log("11.5.5 ", "isStoneSlot_4: " ,isStoneSlot_4);
+           */
+
+          if (isEmpty && (isStoneSlot_1 || isStoneSlot_2 || isStoneSlot_3 || isStoneSlot_4 )) {
+            console.log("11.6.1 ", "====SLOT=TRUE=====");
+            return true;
+          }
+          else {
+            console.log("11.6.2 ", "====SLOT=FALSE=====");
+            return false;
+          }
         }
-        else {
-          console.log("11.6.2 ", "====SLOT=FALSE=====");
-          return false;
-        }
-        ;
 
       },
 
@@ -446,18 +457,21 @@ export class GameComponent  implements OnInit {
 
       invalid: function (el, handle) {
 
-        let isStoneSlot = el.parentElement.parentElement.classList.contains('ship_slots');
-        let isInvalid = isStoneSlot;
+        if (el) {
 
-        if (isInvalid) {
-          console.log("10.10.1 ", "-----TRUE-----");
-          /**/
-          return true;
-        }
-        else {
-          console.log("10.10.2 ", "-----FALSE-----");
-          /**/
-          return false; //false: don't prevent any drags from initiating by default
+          let isStoneSlot = el.parentElement.parentElement.classList.contains('ship_slots');
+          let isInvalid = isStoneSlot;
+
+          if (isInvalid) {
+            console.log("10.10.1 ", "-----TRUE-----");
+            /**/
+            return true;
+          }
+          else {
+            console.log("10.10.2 ", "-----FALSE-----");
+            /**/
+            return false; //false: don't prevent any drags from initiating by default
+          }
         }
 
       },
@@ -467,23 +481,26 @@ export class GameComponent  implements OnInit {
       //copy: true, //false: elements are moved by default, not copied. true: copies el to target and leaves it in source
       copy: function (el, source) {
 
-        let isCopyClass = el.classList.contains('you-may-copy-us'); //the css class name
-        let isCopyId = el.id === '#you-may-copy-me'; //the css id; but use a className for this
+        if (el) {
 
-        /*Debugging
-         console.log("11.15.1 ", `el.id: ${el.id}`); //moved element
-         console.log("11.15.2 ", `el.className: ${el.className}`);
-         console.log("11.15.1 ", `el.isCopyClass: ${isCopyClass}`);
-         console.log("11.15.2 ", `el.isCopyId: ${isCopyId}`);
-         */
+          let isCopyClass = el.classList.contains('you-may-copy-us'); //the css class name
+          let isCopyId = el.id === '#you-may-copy-me'; //the css id; but use a className for this
 
-        if (isCopyClass || isCopyId) {
-          console.log("11.16.1 ", "----COPY-TRUE-----");
-          return true;
-        }
-        else {
-          console.log("11.16.2 ", "----COPY-FALSE-----");
-          return false;
+          /*Debugging
+           console.log("11.15.1 ", `el.id: ${el.id}`); //moved element
+           console.log("11.15.2 ", `el.className: ${el.className}`);
+           console.log("11.15.1 ", `el.isCopyClass: ${isCopyClass}`);
+           console.log("11.15.2 ", `el.isCopyId: ${isCopyId}`);
+           */
+
+          if (isCopyClass || isCopyId) {
+            console.log("11.16.1 ", "----COPY-TRUE-----");
+            return true;
+          }
+          else {
+            console.log("11.16.2 ", "----COPY-FALSE-----");
+            return false;
+          }
         }
 
       },
