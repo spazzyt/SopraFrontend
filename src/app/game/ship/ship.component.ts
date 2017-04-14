d6240b270a892391fddee7a2fe20947a49b31d74
since4 <rol123ben@outlook.de>
2017-04-14 11:12:15 +0200

    clean up files

--- src/app/game/ship/ship.component.ts
+++ src/app/game/ship/ship.component.ts
@@ -41,65 +41,6 @@ export class ShipComponent implements OnInit {
   //Class Methods
   //===============
 
-  //this function is called from within Dragula drop
-  //-----------------------------------------------
-  addStoneToShip(htmlId:string){
-
-    if(1){console.log("shipComponent:addStoneToShip:htmlId: ", htmlId)}
-    //extract data from string
-    let id=Number(htmlId.substring(htmlId.length-1,htmlId.length));
-    let currentShip=Number(htmlId.substring(5,6));
-    let currentStonePositionOnShip=Number(htmlId.substring(12,13));
-    let colour_=htmlId.substring(14,16);
-    let colour:ColourEnum;
-    if(colour_=="bl"){
-      colour=ColourEnum.black;
-    }
-    else if(colour_=="wh"){
-      colour=ColourEnum.white;
-    }
-    else if(colour_=="br"){
-      colour=ColourEnum.brown;
-    }
-    else if(colour_=="gr"){
-      colour=ColourEnum.gray;
-    }
-    else {console.log("shipComponent error")}
-
-    if(1){console.log(": ", htmlId.substring(htmlId.length-1,htmlId.length))}
-    if(1){console.log(": ", htmlId)}
-    if(1){console.log(": ", htmlId.substring(14,16))}
-
-    //TODO: New stone object (better, take one from the sled?)
-    let stone= new Stone(id, colour);
-
-    //fill the array which creates the stone in slot
-    this.stonesOnShip[1][currentStonePositionOnShip-1] = stone;
-  }
-
-
-  //TODO: here again we let Angular 2 add the stones to the site
-  //Returns the array of stones in the ship
-  // (but removes all null values)
-  //---------------------------------------
-  passStonesToSite(){
-    let returnStones;
-
-    for(let i = 0; i < 4; i++){
-      if(this.stonesOnShip[i] != null){
-        returnStones.append(this.stonesOnShip[i]);
-    }
-
-    return returnStones;
-    }
-  }
-
-  //TODO: here again we let Angular 2 delete the stones from the ship
-  //Deletes the contents of the stonesOnShip array
-  // (use this after passing the stones to a site)
-  emptyStoneArray(){
-    this.stonesOnShip = [null, null, null, null];
-  }
 
   //TODO:
   deactivateOrActivateShips(){
