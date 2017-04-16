a6c649347125d2baf1e64478113b5f6207d62246
since4 <rol123ben@outlook.de>
2017-04-16 13:31:15 +0200

    random market card generation, still buggy

--- src/app/shared/models/market-card.ts
+++ src/app/shared/models/market-card.ts
@@ -15,6 +15,9 @@ export class MarketCard {
   //card name
   public name:string="";
 
+  //card name
+  public sloppyName:string="";
+
   //card colour
   public colour:string="";
 
@@ -29,15 +32,39 @@ export class MarketCard {
 
     //initialize attributes
     this.id=id_;
-    this.name=this.idToName(id_);
+    this.name=this.idToRealName(id_);
+    this.sloppyName=this.idToSloppyName(id_);
     this.colour=this.idToColour(id_);
-    this.imageUrl = "../../../assets/images/cards/" + this.name + ".png"
+    this.imageUrl = "../../../assets/images/cards/" + this.sloppyName + ".png"
     //console.log("URL: ", this.imageUrl)
     this.currentPosition=currentPosition_;
 
   }
 
-  idToName(id){
+  /**
+   RED CARDS
+   - Paved Path (2)
+   - Sarcophagus (2)
+   - Entrance (2)
+
+   GREEN CARDS
+   - Pyramid Decoration (2)
+   - Temple Decoration (2)
+   - Burial Chamber Decoration (2)
+   - Obelisk Decoration (2)
+
+   VIOLET CARD
+   - Statue (10)
+
+   BLUE CARDS
+   - Chisel (3)
+   - Lever (2)
+   - Hammer (2)
+   - Sail (3)
+
+   */
+
+  idToSloppyName(id){
     switch (id)
     {
       case 1:
@@ -69,25 +96,25 @@ export class MarketCard {
       case 14:
         return "obeliskdec";
       case 15:
-        return "statue";
+        return "statues";
       case 16:
-        return "statue";
+        return "statues";
       case 17:
-        return "statue";
+        return "statues";
       case 18:
-        return "statue";
+        return "statues";
       case 19:
-        return "statue";
+        return "statues";
       case 20:
-        return "statue";
+        return "statues";
       case 21:
-        return "statue";
+        return "statues";
       case 22:
-        return "statue";
+        return "statues";
       case 23:
-        return "statue";
+        return "statues";
       case 24:
-        return "statue";
+        return "statues";
       case 25:
         return "chisel";
       case 26:
@@ -108,6 +135,85 @@ export class MarketCard {
         return "sail";
       case 34:
         return "sail";
+      default:
+        alert("Error from market-card.ts: idToName(id)");
+        return null;
+    }
+
+  }
+
+
+  idToRealName(id){
+    switch (id)
+    {
+      case 1:
+        return "Paved Path";
+      case 2:
+        return "Paved Path";
+      case 3:
+        return "Sarcophagus";
+      case 4:
+        return "Sarcophagus";
+      case 5:
+        return "Entrance";
+      case 6:
+        return "Entrance";
+      case 7:
+        return "Pyramid Decoration";
+      case 8:
+        return "Pyramid Decoration";
+      case 9:
+        return "Temple Decoration";
+      case 10:
+        return "Temple Decoration";
+      case 11:
+        return "Burial Chamber Decoration";
+      case 12:
+        return "Burial Chamber Decoration";
+      case 13:
+        return "Obelisk Decoration";
+      case 14:
+        return "Obelisk Decoration";
+      case 15:
+        return "Statue";
+      case 16:
+        return "Statue";
+      case 17:
+        return "Statue";
+      case 18:
+        return "Statue";
+      case 19:
+        return "Statue";
+      case 20:
+        return "Statue";
+      case 21:
+        return "Statue";
+      case 22:
+        return "Statue";
+      case 23:
+        return "Statue";
+      case 24:
+        return "Statue";
+      case 25:
+        return "Chisel";
+      case 26:
+        return "Chisel";
+      case 27:
+        return "Chisel";
+      case 28:
+        return "Lever";
+      case 29:
+        return "Lever";
+      case 30:
+        return "Hammer";
+      case 31:
+        return "Hammer";
+      case 32:
+        return "Sail";
+      case 33:
+        return "Sail";
+      case 34:
+        return "Sail";
       default:
         alert("Error from market-card.ts: idToName(id)");
         return null;
