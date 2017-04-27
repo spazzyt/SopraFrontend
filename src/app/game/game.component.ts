2e6d6a3fbc085d0818a2650b24a7bbf8eb116de7
davwys <david.wyss@hotmail.ch>
2017-04-27 18:22:53 +0200

    market card removal works

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1063,7 +1063,6 @@ export class GameComponent  implements OnInit {
 
       //Place stone on ship:
       case PositionEnum.DepartingHarbour:
-        //TODO check if this delivers the proper stone colour
         let stoneToAdd = new Stone(-1, this.nameToColourMap[username]);
         this.ships[move.ShipID].addStoneById(stoneToAdd, move.pos);
 
